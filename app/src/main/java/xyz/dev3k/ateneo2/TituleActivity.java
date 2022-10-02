9818549b63a300bf62466414471f31a36ec8b4e0
Tobias Castañeda <tobias.2000david@gmail.com>
2022-10-02 11:05:23 -0300

    Arreglo pantalla de inicio

---
+++ app/src/main/java/xyz/dev3k/ateneo2/TituleActivity.java
@@ -0,0 +1,14 @@
+package xyz.dev3k.ateneo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TituleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_titule);
+    }
+}
